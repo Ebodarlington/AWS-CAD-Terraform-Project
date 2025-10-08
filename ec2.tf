ae5ac0c2b14fc1212c2b63234982b2bcd5336394
Ebodarlington <ycm.darlington@gmail.com>
2025-10-07 22:35:57 -0400

    Add files via upload

---
+++ ec2.tf
@@ -0,0 +1,12 @@
+
+ resource "aws_instance" "web" {
+  ami           = var.ami
+  instance_type = var.instance_type
+  subnet_id     = aws_subnet.subnet1.id
+  vpc_security_group_ids = [aws_security_group.PublicEC2SG.id]
+
+  tags = {
+    Name = "DatabaseServer"
+  }
+}
+
